cddce99a914c636c975107d3b39afc3c3df6a814
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-12 10:13:09 -0700

    add more code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 function SportsOdds() {
   const [sports, setSports] = useState([]);
   const [searchSport, setSearchSport] = useState("");
+  const [gameType, setGameType] = useState("")
 
   useEffect (()=>{
   const apiUrl ="https://odds.p.rapidapi.com/v4/sports?all=true"
@@ -23,22 +24,26 @@ function SportsOdds() {
 }, []);
 
 function filterSports(){
-  return sports.filter(sport => sport.key.toLowerCase().includes(searchSport.toLowerCase()));
+  return sports.filter(sport => sport.title.toLowerCase().includes(searchSport.toLowerCase()));
 }
 
-function displaySportsOdds(){
-  const filteredSports = filterSports();
-  return(
-    <div id="sports-odds-container">
-      <input type="text"
-          placeholder='search by sport'
-          value={searchSport}
-          onChange= {e => setSearchSport(e.target.value)} />
-
-    </div>
-  )
+function filterGames(){
+  if (!gameType){
+    return [];
+  }
+  return sports.flatMap(sport => sport.events.filter(event => event.type=== gameType));
 }
 
+function handleSearchSportChange(event){
+  setSearchSport(event.target.value);
+}
+
+function handleGameTypeChange(event){
+  setGameType(event.target.value);
+}
+
+const filteredSports = filterSports();
+const filteredGames = filterGames();
 
 
 
